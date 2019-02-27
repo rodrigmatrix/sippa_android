c586d8991c74d5353891b61fc0841aa5e959a759
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-02-27 12:56:02 -0300

    Working

--- TesteApi/teste.js
+++ TesteApi/teste.js
@@ -1,23 +1,11 @@
 async function getMovies() {
-    const url = "https://cors.io/?https://sistemas.quixada.ufc.br/apps/sippa/index.jsp"
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'same-origin',
-    }).then(res => {
-      console.log(res.headers.get('set-cookie')); // undefined
-      console.log(document.cookie); // nope
-      return res.json();
-    }).then(json => {
-      if (json.success) {
-        this.setState({ error: '' });
-        this.context.router.push(json.redirect);
-      }
-      else {
-        this.setState({ error: json.error });
-      }
-    });
+    const url = "https://sistemas.quixada.ufc.br/apps/sippa"
+    const options = {
+        method: 'GET',
+        headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded'
+        })
+    }
+    fetch(url,options)
+    .then(response => console.log(response.text()))
 }
