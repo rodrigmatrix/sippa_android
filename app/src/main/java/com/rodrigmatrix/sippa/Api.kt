3b8499f5f8183e3f7a5f7562d21ad68fd1a37f54
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-04-13 00:00:37 -0300

    Captcha download

--- app/src/main/java/com/rodrigmatrix/sippa/Api.kt
+++ app/src/main/java/com/rodrigmatrix/sippa/Api.kt
@@ -2,6 +2,7 @@ package com.rodrigmatrix.sippa
 
 import android.util.Log
 import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import org.json.JSONObject
 import java.net.URLEncoder
@@ -9,27 +10,40 @@ import java.security.Policy
 
 class Api {
 
-    fun get(){
-        "https://sistemas.quixada.ufc.br/apps/sippa/".httpPost().response{
-                request, response, result ->
-           // Log.d("Message", response.toString())
-        }
-    }
-    fun login(login: String, password: String, captcha: String){
-        val encoded = "login=0421757&senha=iphone5s&conta=aluno&captcha=au55&comando=CmdLogin&enviar=Entrar"
+//    fun get_cookie(): String{
+//        var data = ""
+//        var jsession = ""
+//        "https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg"
+//            .httpGet()
+//            .response{ request, response, result ->
+//                data = response.headers["Set-Cookie"].toString()
+//                println("response cookie " + data)
+//                data = data.replace("[","")
+//                var parts = data.split(";")
+//                jsession = parts[0]
+//                println("JSESSION:= " + jsession)
+//            }
+//
+//            return jsession
+//
+//    }
+
+    fun login(login: String, password: String, captcha: String, cookie: String){
+        val encoded = "login=" + login + "&senha=" + password + "&conta=aluno&captcha=" + captcha + "&comando=CmdLogin&enviar=Entrar"
+        println("cookie login: " + cookie)
+
         "https://sistemas.quixada.ufc.br/apps/ServletCentral"
             .httpPost()
             .timeout(50000)
             .timeoutRead(60000)
             .header("Content-Type" to "application/x-www-form-urlencoded")
-            .header("Cookie", "JSESSIONID=1E606D44155FA6FAD571AA793B8B6A02")
+            .header("Cookie", cookie)
             .body(encoded)
             .timeout(50000)
             .timeoutRead(60000)
             .response{
                 request, response, result ->
-            Log.d("the result is", result.toString())
-            Log.d("the response is", response.toString())
-        }
+                println(response)
+            }
     }
 }
