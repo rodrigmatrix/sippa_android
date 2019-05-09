7235dc2944b88aa27513f942dff4962319dc2a05
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-05-09 00:18:27 -0300

    Gerar novos captchas

--- app/src/main/java/com/rodrigmatrix/sippa/Api.kt
+++ app/src/main/java/com/rodrigmatrix/sippa/Api.kt
@@ -1,14 +1,22 @@
 package com.rodrigmatrix.sippa
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import com.github.kittinunf.fuel.httpPost
+import com.google.android.material.snackbar.Snackbar
+import okhttp3.*
+import java.io.IOException
+import java.lang.Exception
 
 class Api {
     fun Context.toast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_SHORT){
         Toast.makeText(context, message , duration).show()
     }
-    fun login(login: String, password: String, captcha: String, cookie: String){
+    fun login(login: String, password: String, captcha: String, cookie: String, context: Context, view: View, captcha_image: ImageView){
         var encoded = "login=" + login + "&senha=" + password + "&conta=aluno&captcha=" + captcha + "&comando=CmdLogin&enviar=Entrar"
         //println("encoded form: " + encoded)
         var code = 0
@@ -22,17 +30,114 @@ class Api {
             .timeout(50000)
             .timeoutRead(60000)
             .response{ request, response, result ->
-                if(response.toString().contains("Olá ALUNO(A)")){
-                    println("login com sucesso")
-                }
-                else if(response.toString().contains("Erro 500: Contacte o Administrador do Sistema")){
-                    println("Error 500 contacte")
-                }
-                else if(response.toString().contains("Erro ao digitar os caracteres. Por favor, tente novamente.")){
-                    println("Captcha incorreto")
-                }
-                else if(response.toString().contains("Aluno não encontrado ou senha inválida.")){
-                    println("Aluno senha incorreto")
+                when {
+                    response.toString().contains("Olá ALUNO(A)") -> {
+                        val intent = Intent(context, Home::class.java)
+                        intent.putExtra("html_response", response.toString())
+                        context.startActivity(intent)
+                        //println("login com sucesso")
+                    }
+                    response.toString().contains("Erro 500: Contacte o Administrador do Sistema") -> {
+                        val request = Request.Builder()
+                            .url("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
+                            .build()
+                        val client = OkHttpClient()
+                        var jsession = "error"
+                        client.newCall(request).enqueue(object : Callback {
+                            override fun onResponse(call: Call, response: Response) {
+                                var data = response.header("Set-Cookie").toString()
+                                data = data.replace("[","")
+                                var parts = data.split(";")
+                                jsession = parts[0]
+                                //println("COOKIE " + jsession)
+                                if(response.body() != null) {
+                                    var bmp = BitmapFactory.decodeStream(response.body()!!.byteStream())
+                                    try {
+                                        if(bmp != null) {
+                                            captcha_image.setImageBitmap(bmp)
+                                        }
+                                    } catch (e: Exception) {
+                                        println(e.message)
+                                    }
+                                }
+                            }
+                            //@Throws(IOException::class)
+                            override fun onFailure(call: Call, e: IOException) {
+                                println(e.message)
+                            }
+                        })
+                        val snackbar = Snackbar.make(view, "Tempo de conexão expirado. Digite o novo captcha", Snackbar.LENGTH_LONG)
+                        snackbar.show()
+
+                        //println("Error 500 contacte")
+                    }
+                    response.toString().contains("Erro ao digitar os caracteres. Por favor, tente novamente.") -> {
+                        val request = Request.Builder()
+                            .url("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
+                            .build()
+                        val client = OkHttpClient()
+                        var jsession = "error"
+                        client.newCall(request).enqueue(object : Callback {
+                            override fun onResponse(call: Call, response: Response) {
+                                var data = response.header("Set-Cookie").toString()
+                                data = data.replace("[","")
+                                var parts = data.split(";")
+                                jsession = parts[0]
+                                //println("COOKIE " + jsession)
+                                if(response.body() != null) {
+                                    var bmp = BitmapFactory.decodeStream(response.body()!!.byteStream())
+                                    try {
+                                        if(bmp != null) {
+                                            captcha_image.setImageBitmap(bmp)
+                                        }
+                                    } catch (e: Exception) {
+                                        println(e.message)
+                                    }
+                                }
+                            }
+                            //@Throws(IOException::class)
+                            override fun onFailure(call: Call, e: IOException) {
+                                println(e.message)
+                            }
+                        })
+                        val snackbar = Snackbar.make(view, "Captcha incorreto. Digite o novo captcha", Snackbar.LENGTH_LONG)
+                        snackbar.show()
+                        //println("Captcha incorreto")
+                    }
+                    response.toString().contains("Aluno não encontrado ou senha inválida.") -> {
+                        val request = Request.Builder()
+                            .url("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
+                            .build()
+                        val client = OkHttpClient()
+                        var jsession = "error"
+                        client.newCall(request).enqueue(object : Callback {
+                            override fun onResponse(call: Call, response: Response) {
+                                var data = response.header("Set-Cookie").toString()
+                                data = data.replace("[","")
+                                var parts = data.split(";")
+                                jsession = parts[0]
+                                //println("COOKIE " + jsession)
+                                if(response.body() != null) {
+                                    var bmp = BitmapFactory.decodeStream(response.body()!!.byteStream())
+                                    try {
+                                        if(bmp != null) {
+                                            captcha_image.setImageBitmap(bmp)
+                                        }
+                                    } catch (e: Exception) {
+                                        println(e.message)
+                                    }
+                                }
+                            }
+                            //@Throws(IOException::class)
+                            override fun onFailure(call: Call, e: IOException) {
+                                println(e.message)
+                            }
+                        })
+                        val snackbar = Snackbar.make(view, "Aluno ou senha não encontrados", Snackbar.LENGTH_LONG)
+                        snackbar.show()
+                        //println("Aluno senha incorreto")
+                    }
+                    //println(response.toString())
                 }
                 //println(response.toString())
             }
--- app/src/main/java/com/rodrigmatrix/sippa/MainActivity.kt
+++ app/src/main/java/com/rodrigmatrix/sippa/MainActivity.kt
@@ -10,78 +10,84 @@ import java.io.IOException
 import android.graphics.BitmapFactory
 import okhttp3.*
 import java.lang.Exception
-
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val loginbtn = findViewById<View>(R.id.login) as Button
-        val captcha = findViewById<View>(R.id.captcha_image) as ImageView
+        val captcha_image = findViewById<View>(R.id.captcha_image) as ImageView
         val api = Api()
-//        Picasso.get()
-//            .load("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
-//            .into(captcha)
         val request = Request.Builder()
             .url("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
             .build()
         val client = OkHttpClient()
-        var data = ""
+        var jsession = "error"
         client.newCall(request).enqueue(object : Callback {
-
             override fun onResponse(call: Call, response: Response) {
-                data = response.header("Set-Cookie").toString()
-
-                println("res mes " + data)
+                var data = response.header("Set-Cookie").toString()
+                data = data.replace("[","")
+                var parts = data.split(";")
+                jsession = parts[0]
+                //println("COOKIE " + jsession)
                 if(response.body() != null) {
                     var bmp = BitmapFactory.decodeStream(response.body()!!.byteStream())
-
                     try {
                         if(bmp != null) {
-                            captcha.setImageBitmap(bmp)
+                            captcha_image.setImageBitmap(bmp)
                         }
                     } catch (e: Exception) {
                         println(e.message)
                     }
-
-
                 }
             }
-
-            @Throws(IOException::class)
+            //@Throws(IOException::class)
             override fun onFailure(call: Call, e: IOException) {
                 println(e.message)
             }
         })
-
-        //getCaptcha(captcha)
-
-
         loginbtn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-
                 var login = findViewById<EditText>(R.id.login_input)
                 var password = findViewById<EditText>(R.id.password_input)
                 var captcha_input = findViewById<EditText>(R.id.captcha_input)
 
-                data = data.replace("[","")
-                var parts = data.split(";")
-                var jsession = parts[0]
-                // status 200:  aluno ou senhanao encontrado
-                // status 200: erro 500 contacte o adm do sistema (cookie expirado)
-                // status 200: erro ao digitar captcha
-                // status 200: sucesso
-                api.login(login.text.toString(), password.text.toString(), captcha_input.text.toString(), jsession)
-
-
+                api.login(login.text.toString(), password.text.toString(), captcha_input.text.toString(), jsession, this@MainActivity, findViewById(R.id.activity_main), captcha_image)
             }
         })
     }
 }
 
-fun getCaptcha(captcha: ImageView) {
-
+fun getCaptcha(captcha: ImageView): String {
+    val request = Request.Builder()
+        .url("https://sistemas.quixada.ufc.br/apps/sippa/captcha.jpg")
+        .build()
+    val client = OkHttpClient()
+    var jsession = "error"
+    client.newCall(request).enqueue(object : Callback {
+        override fun onResponse(call: Call, response: Response) {
+            var data = response.header("Set-Cookie").toString()
+            data = data.replace("[","")
+            var parts = data.split(";")
+            jsession = parts[0]
+            println("COOKIE " + jsession)
+            if(response.body() != null) {
+                var bmp = BitmapFactory.decodeStream(response.body()!!.byteStream())
+                try {
+                    if(bmp != null) {
+                        captcha.setImageBitmap(bmp)
+                    }
+                } catch (e: Exception) {
+                    println(e.message)
+                }
+            }
+        }
+        //@Throws(IOException::class)
+        override fun onFailure(call: Call, e: IOException) {
+            println(e.message)
+        }
+    })
+    return jsession
 }
-
-
