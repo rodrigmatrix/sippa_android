4d321d2571f49dfb494bb8d8b92ba3232b56b613
rodrigmatrix <rodrigmatrix@gmail.com>
2019-06-10 22:48:12 -0300

    Fix crash ao tentar setar nome  e matricula

--- app/src/main/java/com/rodrigmatrix/sippa/Fragments/DisciplinasFragment.kt
+++ app/src/main/java/com/rodrigmatrix/sippa/Fragments/DisciplinasFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.google.android.material.snackbar.Snackbar
--- app/src/main/java/com/rodrigmatrix/sippa/HomeActivity.kt
+++ app/src/main/java/com/rodrigmatrix/sippa/HomeActivity.kt
@@ -5,10 +5,12 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.Menu
 import androidx.core.view.GravityCompat
 import androidx.appcompat.app.ActionBarDrawerToggle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -24,10 +27,14 @@ import com.rodrigmatrix.sippa.persistance.StudentsDatabase
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 import kotlinx.android.synthetic.main.nav_header_home.*
+import kotlinx.android.synthetic.main.nav_header_home.view.*
 import org.jetbrains.anko.configuration
 import org.jetbrains.anko.textColor
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import org.jetbrains.anko.linearLayout
 
 
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, HorasFragment.OnFragmentInteractionListener, DisciplinasFragment.OnFragmentInteractionListener, CoroutineScope {
@@ -47,6 +54,12 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
         nav_view.setCheckedItem(R.id.disciplinas_select)
+        database = Room.databaseBuilder(
+            applicationContext,
+            StudentsDatabase::class.java, "database.db")
+            .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
+            .build()
         toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
@@ -63,17 +76,10 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
         title = "Disciplinas"
-        database = Room.databaseBuilder(
-            applicationContext,
-            StudentsDatabase::class.java, "database.db")
-            .fallbackToDestructiveMigration()
-            .allowMainThreadQueries()
-            .build()
-        Thread{
+
             val student = database.studentDao().getStudent()
             runOnUiThread {
-                student_name_text.text = student.name
-                student_matricula_text.text = student.matricula
+
                 when(student.theme){
                     "light" -> {
                         setDefaultNightMode(MODE_NIGHT_NO)
@@ -88,7 +94,6 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     }
                 }
             }
-        }.start()
         dialogPassword(database)
         fragmentManager = supportFragmentManager
         for(fragment in fragmentManager.fragments){
@@ -98,56 +103,65 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         fragmentManager.beginTransaction().add(R.id.view_disciplinas, horasFragment).hide(horasFragment).commit()
         fragmentManager.beginTransaction().add(R.id.view_disciplinas, infoFragment).hide(infoFragment).commit()
         selectedFragment = disciplinasFragment
+        launch(handler) {
+            runOnUiThread {
+                nav_view.getHeaderView(0).findViewById<TextView>(R.id.student_name_text).text = student.name
+                nav_view.getHeaderView(0).findViewById<TextView>(R.id.student_matricula_text).text = student.matricula
+            }
+        }
     }
     private fun dialogPassword(database: StudentsDatabase){
-            if(database.studentDao().getStudent().login == ""){
-                var student = database.studentDao().getStudent()
-                runOnUiThread {
-                    var dialog = AlertDialog.Builder(this@HomeActivity)
-                    dialog.setTitle("Salvar Dados")
-                    dialog.setMessage("Deseja salvar seus dados de login?")
-                    dialog.setPositiveButton("Sim") { dialog, which ->
-                        var login = intent.getStringExtra("login")
-                        var password = intent.getStringExtra("password")
-                        student.login = login
-                        student.password = password
-                        Thread {
-                            database.studentDao().delete()
-                            database.studentDao().insert(student)
-                        }.start()
-                    }
-                    dialog.setNegativeButton("Agora Não") { dialog, which ->
-                    }
-                    var alert = dialog.create()
-                    alert.show()
-                }
-            }
-            else{
-                var student = database.studentDao().getStudent()
-                runOnUiThread {
+        if(database.studentDao().getStudent().login == ""){
+            var student = database.studentDao().getStudent()
+            runOnUiThread {
+                var dialog = AlertDialog.Builder(this@HomeActivity)
+                dialog.setTitle("Salvar Dados")
+                dialog.setMessage("Deseja salvar seus dados de login?")
+                dialog.setPositiveButton("Sim") { dialog, which ->
                     var login = intent.getStringExtra("login")
                     var password = intent.getStringExtra("password")
-                    if(student.login != login || student.password != password){
-                        runOnUiThread{
-                            var dialog = AlertDialog.Builder(this@HomeActivity)
-                            dialog.setTitle("Atualizar Login")
-                            dialog.setMessage("Você fez login com uma nova conta. Deseja atualizar os dados salvos de login?")
-                            dialog.setPositiveButton("Atualizar") { dialog, which ->
-                                student.login = login
-                                student.password = password
-                                Thread {
-                                    database.studentDao().delete()
-                                    database.studentDao().insert(student)
-                                }.start()
-                            }
-                            dialog.setNegativeButton("Agora Não") { dialog, which ->
-                            }
-                            var alert = dialog.create()
-                            alert.show()
+                    student.login = login
+                    student.password = password
+                    Thread {
+                        database.studentDao().delete()
+                        database.studentDao().insert(student)
+                    }.start()
+                }
+                dialog.setNegativeButton("Agora Não") { dialog, which ->
+                }
+                var alert = dialog.create()
+                alert.show()
+            }
+        }
+        else{
+            var student = database.studentDao().getStudent()
+            runOnUiThread {
+                var login = intent.getStringExtra("login")
+                var password = intent.getStringExtra("password")
+                if(student.login != login || student.password != password){
+                    runOnUiThread{
+                        var dialog = AlertDialog.Builder(this@HomeActivity)
+                        dialog.setTitle("Atualizar Login")
+                        dialog.setMessage("Você fez login com uma nova conta. Deseja atualizar os dados salvos de login?")
+                        dialog.setPositiveButton("Atualizar") { dialog, which ->
+                            student.login = login
+                            student.password = password
+                            Thread {
+                                database.studentDao().delete()
+                                database.studentDao().insert(student)
+                            }.start()
                         }
+                        dialog.setNegativeButton("Agora Não") { dialog, which ->
+                        }
+                        var alert = dialog.create()
+                        alert.show()
                     }
                 }
             }
+        }
+    }
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        Log.e("Exception", ":$throwable")
     }
     override fun onStop() {
         job.cancel()
